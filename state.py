26470b64550738226a175910c1348cf57bf1c6f1
Gloriel621 <gloriel621@gmail.com>
2021-11-17 17:43:06 +0900

    move State to state.py

---
+++ state.py
@@ -0,0 +1,21 @@
+import numpy as np
+from data import Hand, Buyers
+
+
+class State:
+    def __init__(self):
+        self.hand = Hand
+        self.buyers = Buyers
+
+        self.state_to_numpy()
+
+    def state_to_numpy(self):  # change to model input form
+        np_hand = np.fromiter(self.hand.values(), dtype=int)
+
+        np_buyers = []
+        for buyer in self.buyers:
+            np_buyers.append(np.fromiter(buyer.values(), dtype=int))
+
+        self.state = np_hand
+        for np_buyer in np_buyers:
+            self.state = np.concatenate([self.state, np_buyer], dtype=int)

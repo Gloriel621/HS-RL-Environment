e7630ba4b63068e6d4029a5ec3263eda934d24bd
Gloriel621 <gloriel621@gmail.com>
2021-11-17 11:34:37 +0900

    update data

---
+++ data.py
@@ -0,0 +1,77 @@
+Baloon_Merchant = [
+    ("gold", "healing_potion", 2),
+    ("hand_axe", "golden_goblet", 5),
+    ("linen_bandage", "jade_locket", 2),
+    ("stormwind_cheddar", "alliance_mace", 14),
+    ("cute_doll", "draught_of_angels", 3),
+    ("shadow_gem", "gilneas_dagger", 2),
+    ("elixir_of_vigor", "loyal_pet_whistle", 4),
+]
+Armor_Vender = [
+    ("gold", "iron_dagger", 1),
+    ("stormwind_cheddar", "jade_locket", 4),
+    ("healing_potion", "golden_goblet", 4),
+    ("hand_axe", "ruby_crown", 22),
+    ("potions_of_night", "sphere_of_wisdom", 4),
+    ("gnomish_shield", "shadow_gem", 3),
+    ("loyal_pet_whistle", "sapphire_wand", 2),
+]
+Barrens_Blacksmith = [
+    ("gold", "hand_axe", 2),
+    ("linen_bandage", "cute_doll", 5),
+    ("very_nice_hat", "arcane_scroll", 8),
+    ("angry_cristal", "draught_of_angels", 1),
+    ("jade_locket", "potions_of_night", 5),
+    ("goblin_fishing_pole", "everburning_candle", 4),
+    ("captivating_pipe", "tiger_amulet", 5),
+]
+
+Darkshire_Alchemist = [
+    ("gold", "captivating_pipe", 11),
+    ("elixir_of_vigor", "linen_bandage", 1),
+    ("healing_potion", "gilneas_dagger", 49),
+    ("iron_dagger", "gnomish_shield", 12),
+    ("stormwind_cheddar", "potions_of_night", 13),
+    ("arcane_scroll", "tiger_amulet", 3),
+    ("golden_goblet", "alliance_mace", 3),
+]
+
+Shady_dealer = [
+    ("gold", "arcane_scroll", 25),
+    ("hand_axe", "very_nice_hat", 2),
+    ("healing_potion", "captivating_pipe", 7),
+    ("elixir_of_vigor", "angry_cristal", 20),
+    ("sapphire_wand", "gilneas_dagger", 2),
+    ("golden_goblet", "sphere_of_wisdom", 10),
+    ("stormwind_cheddar", "sapphire_wand", 15),
+]
+
+Master_Swordsmith = [
+    ("gold", "elixir_of_vigor", 3),
+    ("hand_axe", "goblin_fishing_pole", 4),
+    ("very_nice_hat", "sapphire_wand", 5),
+    ("alliance_mace", "everburning_candle", 1),
+    ("cute_doll", "angry_cristal", 5),
+    ("captivating_pipe", "ruby_crown", 3),
+    ("golden_goblet", "draught_of_angels", 9),
+]
+
+Drakkari_Enchanter = [
+    ("gold", "stormwind_cheedar", 2),
+    ("stormwind_cheedar", "goblin_fishing_pole", 5),
+    ("iron_dagger", "loyal_pet_whistle", 7),
+    ("elixir_of_vigor", "shadow_gem", 9),
+    ("ruby_crown", "gilneas_dagger", 1),
+    ("gnomish_shield", "tiger_amulet", 4),
+    ("cute_doll", "alliance_mace", 3),
+]
+
+Merchants = [
+    Baloon_Merchant,
+    Armor_Vender,
+    Barrens_Blacksmith,
+    Darkshire_Alchemist,
+    Shady_dealer,
+    Master_Swordsmith,
+    Drakkari_Enchanter,
+]
